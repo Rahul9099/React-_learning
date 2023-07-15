cdcfd0c5957da8d7477e155b32f77e700759e998
Rahul9099 <rahuldange208@gmail.com>
2023-07-15 16:44:51 +0530

    passing variable to compoents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
+const user={
+  name:'rahul',
+  age:32,
+  gender:'male'
+}
+
+function User ({name,age,gender}){
+return (
+  <>
+  <h1>{name}</h1>
+  <h1>{age}</h1>
+  <h1>{gender}</h1>
+  </>
+)
+}
+
 function App() {
   return (
     <div className="App">
-     
+     <User {...user}/>
+     <h1>rahul</h1>
     </div>
   );
 }
