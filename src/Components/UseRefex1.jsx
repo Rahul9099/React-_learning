a805e2f35b7e03a09248bb3eb9abaa6428e7bf4c
Rahul9099 <rahuldange208@gmail.com>
2023-07-17 10:34:30 +0530

    learn about useRef hook that it use to take value retain its value before and after render and can be use when retaing a value but not necessary to show

---
+++ src/Components/UseRefex1.jsx
@@ -0,0 +1,28 @@
+import React, { useRef, useState } from 'react'
+
+function UseRefex1() {
+    const setTime = useRef(null);
+    const [now,setNow] =useState();
+    let second = 0;
+    const stopRef= useRef(null);
+
+    function start(){
+        setTime.current = Date.now();
+        stopRef = setInterval(() => {
+            setNow(Date.now());
+        }, 10);
+    }
+    function stop(){
+    clearInterval(stopRef);
+    }
+    second = (now - setTime.current)/1000;
+  return (
+    <div>
+        <h1>{second.toFixed(3)}</h1>
+          <button onClick={start}>start</button>
+          <button onClick={stop}>stop</button>
+    </div>
+  )
+}
+
+export default UseRefex1
